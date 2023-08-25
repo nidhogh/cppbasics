ef7e9a3ca821a7bb95149201281a8324164896e4
wangjian <1536022504@qq.com>
2023-08-25 22:28:21 +0800

    backup

--- Effective C++ 07.cpp
+++ Effective C++ 07.cpp
@@ -1,9 +1,12 @@
+#include <string>
+
 // Effective C++条款07
 class TimeKeeper
 {
 public:
     TimeKeeper();
     virtual ~TimeKeeper();
+    // ...
 };
 
 class AtomicClock : public TimeKeeper
@@ -18,9 +21,29 @@ class WristWatch : public TimeKeeper
 
 TimeKeeper *getTimeKeeper(); // 返回一个指针，指向一个TimeKeeper派生类的动态分配对象
 
-int main()
+class SpecialString : public std::string
+{ // 馊主意！std::string有个non-virtual析构函数
+    // ...
+};
+
+class AWOV {    //AWOV="Abstract w/o Virtuals"
+public:
+    virtual ~AWOV() = 0;    //声明pure virtual析构函数
+};
+
+AWOV::~AWOV() {}    //pure virtual析构函数的定义
+
+int
+main()
 {
-    TimeKeeper *ptk = getTimeKeeper(); //从TimeKeeper继承体系获得一个动态分配对象。
-    
-    delete ptk; //释放它，避免资源泄露
+    TimeKeeper *ptk = getTimeKeeper(); // 从TimeKeeper继承体系获得一个动态分配对象。
+    // ...
+    delete ptk; // 释放它，避免资源泄露
+
+    // SpecialString* pss = new SpecialString("Impending Doom");
+    // std::string* ps;
+    // ...
+    // ps = pss;       //SpecialString* => std::string*
+    // ...
+    // delete ps;      //未有定义！现实中*ps的SpecialString资源会泄露，因为SpecialString析构函数没被调用
 }
